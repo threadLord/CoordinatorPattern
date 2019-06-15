8f1c1049206a629cfa3353b415e76570a112b018
mare25 <markomutavdzic@gmail.com>
2019-06-15 11:11:13 +0200

    Initial Commit

---
+++ CoordinatorPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by OSX on 6/15/19.
+//  Copyright © 2019 OSX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
